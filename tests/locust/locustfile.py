b9c1641283e149cb708b8046b6f88b4cb99ecd8b
Joe Chin <joe@wwwhoosh.com>
2024-09-14 17:10:16 +0800

    Update locustfile

--- tests/locust/locustfile.py
+++ tests/locust/locustfile.py
@@ -1,6 +1,7 @@
 from locust import HttpUser, task, between
-from random import randrange
 from bs4 import BeautifulSoup
+import random
+import string
 
 def is_static_file(file):
   matches = ['/sites/default/files', '/themes/']
@@ -10,19 +11,23 @@ def is_static_file(file):
     return False
 
 def fetch_static_assets(session, response):
-    """Determine if a URL in the web page is a static asset and should be downloaded."""
-    resource_urls = set()
-    soup = BeautifulSoup(response.text, "html.parser")
+  """Determine if a URL in the web page is a static asset and should be downloaded."""
+  resource_urls = set()
+  soup = BeautifulSoup(response.text, "html.parser")
 
-    for res in soup.find_all(src=True):
-        url = res['src']
-        if is_static_file(url):
-            resource_urls.add(url)
-        else:
-            print(f'Skipping: {url}')
+  for res in soup.find_all(src=True):
+    url = res['src']
+    if is_static_file(url):
+      resource_urls.add(url)
+    else:
+      print(f'Skipping: {url}')
 
-    for url in set(resource_urls):
-        session.client.get(url, name="(Static File)")
+  for url in set(resource_urls):
+    session.client.get(url, name="(Static File)")
+
+def random_string(length=8):
+  random_string = ''.join(random.choices(string.ascii_letters, k=length))
+  return str(random_string)
 
 
 class LoadTest(HttpUser):
@@ -40,37 +45,56 @@ class LoadTest(HttpUser):
     except FileNotFoundError:
         print(f"File {filename} does not exist.")
 
-  @task
-  def page_1(self):
-    """Assume all users start their browsing from the homepage"""
+  @task(100)
+  def homepage(self):
+    """Most users go to the home page"""
     response = self.client.get("/")
     fetch_static_assets(self, response) 
 
-  @task
+  @task(50)
   def page_2(self):
-    """User 2nd random page visit"""
-    index = randrange(self.count)
+    """Some users will visit a 2nd random page"""
+    index = random.randrange(self.count)
     response = self.client.get(self.urls[index])
     fetch_static_assets(self, response) 
 
-  @task
+  @task(25)
   def page_3(self):
-    """User 3rd random page visit"""
-    index = randrange(self.count)
+    """Fewer Users will visit a 3rd random page"""
+    index = random.randrange(self.count)
     response = self.client.get(self.urls[index])
     fetch_static_assets(self, response) 
 
-  @task
-  def page_4(self):
-    """User 4th random page visit"""
-    index = randrange(self.count)
-    response = self.client.get(self.urls[index])
-    fetch_static_assets(self, response) 
-  
-  @task
-  def page_5(self):
-    """User 5th random page visit"""
-    index = randrange(self.count)
-    response = self.client.get(self.urls[index])
-    fetch_static_assets(self, response) 
+  @task(10)
+  def register(self):
+    """A few users will register for a free cookbook"""
 
+    # Visit registration page and get form build id.
+    response = self.client.get("/free-cookbook", name="register")
+    fetch_static_assets(self, response) 
+    soup = BeautifulSoup(response.text, "html.parser")
+    drupal_form_id = soup.select('input[name="form_build_id"]')[0]["value"]
+
+    # Generate random strings
+    first_name = random_string()
+    last_name = random_string()
+    email = f'{first_name}_{last_name}@test.com'
+    address = random_string(15)
+    city = random_string(10)
+    postal_code = random_string(5)
+
+    # Submit form
+    resp = self.client.post("/free-cookbook", {
+      'form_build_id': drupal_form_id,
+      'first_name': first_name,
+      'last_name': last_name,
+      'email': email,
+      'address[address]': address,
+      'address[city]': city,
+      'address[state_province]': '',
+      'address[postal_code]': postal_code,
+      'address[country]': 'Singapore',
+      'form_id': 'webform_submission_cookbook_registration_add_form',
+      'op': 'Submit'
+    })
+    
